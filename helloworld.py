d09049d9535a3fae3bfaf1903d38df3a40b9d775
pukuku101 <113128284+pukuku101@users.noreply.github.com>
2022-09-12 09:30:50 -0700

    Created Hello World

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+print("Hello World")
+import turtle as trtl
+
+painter = trtl.Turtle()
+
+painter.goto(-50,-50)
+painter.circle(10)
+painter.forward(100)
+painter.right(90)
+wn = trtl.Screen()
+wn.mainloop()
