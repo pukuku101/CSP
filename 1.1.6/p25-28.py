209244307260d65b03c330d2a7d2c3f25abfe880
pukuku101 <113128284+pukuku101@users.noreply.github.com>
2022-09-29 09:29:54 -0700

    Create p25-28.py

---
+++ 1.1.6/p25-28.py
@@ -0,0 +1,20 @@
+import turtle as trtl
+painter = trtl.Turtle()
+# Spider abdomen
+painter.pensize(40)
+painter.circle(20)
+# Configure legs
+legs = 8
+leg_length = 70
+angle = 360 / legs
+painter.pensize(5)
+rep = 0
+# Drawing legs
+while (rep < legs):
+  painter.goto(0,20)
+  painter.setheading(angle*rep)
+  painter.forward(leg_length)
+  rep = rep + 1
+painter.hideturtle()
+wn = trtl.Screen()
+wn.mainloop()
