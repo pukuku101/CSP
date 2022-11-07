b1ed31532c37231eadb804b1ac51cd1171cf74c5
pukuku101 <113128284+pukuku101@users.noreply.github.com>
2022-11-07 09:01:57 -0800

    Update catching_turtles.py

--- Chapter1.2/catching_turtles.py
+++ Chapter1.2/catching_turtles.py
@@ -1,12 +1,15 @@
 #-----import statements-----
 import turtle as trtl
 import random as rand
+import leaderboard as lb
 
 #-----game configuration----
 font_setup = ("Arial", 20, "normal")
-timer = 30
+timer = 5
 counter_interval = 1000   #1000 represents 1 second
 timer_up = False
+leaderboard_file_name = "a122_leaderboard.txt"
+player_name=input("Name?")
 #little things
 font_setup=("Arial",20,"normal")
 spot_color = "pink"
@@ -42,10 +45,27 @@ def countdown():
   if timer <= 0:
     counter.write("Time's Up", font=font_setup)
     timer_up = True
+    manage_leaderboard()
   else:
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
     counter.getscreen().ontimer(countdown, counter_interval)
+def manage_leaderboard():
+
+  global score
+  global spot
+
+  # get the names and scores from the leaderboard file
+  leader_names_list = lb.get_names(leaderboard_file_name)
+  leader_scores_list = lb.get_scores(leaderboard_file_name)
+
+  # show the leaderboard with or without the current player
+  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, spot, score)
+
+  else:lb.draw_leaderboard(False, leader_names_list, leader_scores_list, spot, score)
+
 #-----initialize turtle-----
 spot = trtl.Turtle()
 score_writer= trtl.Turtle()
