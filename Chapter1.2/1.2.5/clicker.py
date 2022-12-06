61309abf48007ae662e65f52e7e9349b01ba4aba
pukuku101 <113128284+pukuku101@users.noreply.github.com>
2022-12-06 09:58:17 -0800

    Update clicker.py

--- Chapter1.2/1.2.5/clicker.py
+++ Chapter1.2/1.2.5/clicker.py
@@ -7,9 +7,11 @@ clicks=0
 cpc=1
 away_clicks=0
 cur="200.gif"
-price=5
+font_setup=("Arial",20,"normal")
+item_list=[]
+item_price=[5,10]
+price=item_price.pop(0)
 ###------Function Definitions-----###
-
 def automatic_income():
     global clicks
     clicks=clicks + away_clicks
@@ -17,23 +19,48 @@ def automatic_income():
 def cursor_clicked(x,y):
     global clicks
     clicks=clicks + cpc
-    print(clicks)
-def item_clicked():
+    score_change()
+
+def item_clicked(x,y):
+    global clicks
+    global price
+    global cpc
+    
     if (clicks>=price):
+        price2=round(price*1.8)
+        item_price.insert(0,price2)
         clicks= clicks-price
         cpc=cpc+1
-        price=price*1.2
+        score_change()
+        price=item_price.pop(0)
+    print_description(item)
 
-###------Initializer--------------###
+def score_change():
+    score_counter.clear()
+    score_counter.write("Clicks: " + str(clicks),font=font_setup)
+def print_description(active_turtle):
+    active_turtle.clear()
+    active_turtle.write("Cost: "+str(price),font=font_setup)
+###------Initializer--------###
 wn=trtl.Screen()
 wn.addshape(cur)
 wn.bgpic("giphy.gif")
+###-------Turtles--------###
 cursor=trtl.Turtle(shape=cur)
 item=trtl.Turtle()
+score_counter=trtl.Turtle()
+score_counter.hideturtle()
+score_counter.pu()
+score_counter.goto(-300,300)
+score_counter
 
+
+###-------Shop--------###
 item.pu()
-item.goto(200,200)
-###-------Events------------_####
+item.goto(300,200)
+print_description(item)
+###-------Events------------_###
 cursor.onclick(cursor_clicked)
+item.onclick(item_clicked)
 wn.ontimer(automatic_income, clickps)
 wn.mainloop()
