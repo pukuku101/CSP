71afa6a2958878a77643969104057ecc58b9e4b0
pukuku101 <113128284+pukuku101@users.noreply.github.com>
2023-04-24 17:12:52 -0700

    Update violent.py

--- violent.py
+++ violent.py
@@ -1,21 +1,30 @@
 import matplotlib.pyplot as plt
 violentcrimesfile=open('violentcrimes.txt')
 crimelist = violentcrimesfile.readlines()
-alcholabusefile=open("alcholabuse.txt")
-abuselist=alcholabusefile.readlines()
+alcholabusefile=open("foodprice.txt")
+pricelist=alcholabusefile.readlines()
 print(crimelist)
-print(abuselist)
-"""
+print(pricelist)
+seafood=[]
 for i in range(len(crimelist)):
     crimelist[i]=int(crimelist[i])
-for i in range(len(abuselist)):
-    abuselist[i]=int(abuselist[i])
+for i in range(len(pricelist)):
+    pricelist[i]=int(pricelist[i])
+for i in range(len(pricelist)):
+    tempreverse=pricelist.pop()
+    seafood.append(tempreverse)
 print(crimelist)
-plt.plot(range(1999,2019),crimelist,'b.-')
-plt.axis([1999,2020,0,2000000])
-plt.plot(range(2002,2018),abuselist,'r.--)
+fig, axs=plt.subplots(2)
+fig.suptitle("Rates of Robbery V.S. Reported Net Worth of Imported Sea Food In 100,000's")
+axs[0].plot(range(2000,2020),crimelist,'b.-')
+axs[0].axis([2000,2020,200000,500000])
+axs[1].plot(range(2000,2020),seafood,'r.--')
+axs[1].axis([2000,2020,9000,250000])
 plt.xlabel("Years")
-plt.ylabel("Number in Millions")
-plt.title("Violent Crimes Committed in the U.S. Every Year")
+if (plt.waitforbuttonpress()==True):
+    plt.suptitle("This correlation is undeniably important.")
+if (plt.waitforbuttonpress()==True):
+    plt.suptitle("Did You Expect More?")
 plt.show()
-"""
+violentcrimesfile.close()
+alcholabusefile.close()
